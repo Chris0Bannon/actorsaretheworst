a50c6f67827f0efc4b877ad10113cafeeca988f9
Christopher O'Bannon <Crobwan@gmail.com>
2019-08-13 14:47:09 -0500

    hey look i can do a thing with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  // constructor() {
+  //     super();
+  //     this.state = {user: 'Chris'};
+
+  // }
+  state = {
+    user: "",
+    location: ""
+  }
+  handleChange = (event) => {
+    console.log(event.target.value);
+
+    this.setState({user:event.target.value});
+  }
+
+otherChange = (event) => {
+  this.setState({location:event.target.value})
+}
+clickFunction = (event) => {
+  console.log(this.state);
+  
+}
+
   render() {
+    console.log('RENDER WAS CALLED');
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input placeholder = "enter user name" onChange={this.handleChange}/>
+        <input placeholder = "enter user location" onChange ={this.otherChange}/>
+     You typed: {this.state.user} is from {this.state.location}
+     <button onClick = {this.clickFunction}>push me i am a button</button>
       </div>
     );
   }
