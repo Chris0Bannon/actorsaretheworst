aa4a0192fde39ece507117878170f6de292e8c27
Christopher O'Bannon <Crobwan@gmail.com>
2019-08-13 16:47:40 -0500

    more notees

--- src/App.js
+++ src/App.js
@@ -10,21 +10,37 @@ class App extends Component {
 
   // }
   state = {
-    user: "",
-    location: ""
+    user: {
+      name: '',
+      city: ''
+    }
   }
   handleChange = (event) => {
     console.log(event.target.value);
 
-    this.setState({user:event.target.value});
+    this.setState({user:
+        {...this.state.user,
+        name:event.target.value}});
   }
 
 otherChange = (event) => {
-  this.setState({location:event.target.value})
+  this.setState({user:
+               {...this.state.user,
+                city: event.target.value}})
 }
 clickFunction = (event) => {
   console.log(this.state);
   
+}
+
+handleChangeFor = (event, propertyName) => {
+  this.setState({
+    user:
+    {
+      ...this.state.user,
+      [propertyName]: event.target.value
+    }
+  })
 }
 
   render() {
@@ -32,9 +48,9 @@ clickFunction = (event) => {
     
     return (
       <div className="App">
-        <input placeholder = "enter user name" onChange={this.handleChange}/>
-        <input placeholder = "enter user location" onChange ={this.otherChange}/>
-     You typed: {this.state.user} is from {this.state.location}
+        <input placeholder = "enter user name" onChange={(event) => {this.handleChangeFor(event, 'name')}}/>
+        <input placeholder = "enter user location" onChange ={(event) => {this.handleChangeFor(event, 'city')}}/>
+     You typed: {this.state.user.name} is from {this.state.user.city}
      <button onClick = {this.clickFunction}>push me i am a button</button>
       </div>
     );
---
+++ src/spread.js
@@ -0,0 +1,14 @@
+const numberList = [5, 3, 1];
+
+const newNumber = 7;
+
+// const newNumberList = [numberList[0], numberList[1], numberList[2], newNumber]
+
+const newNumberList = [ 
+    ...numberList, newNumber
+];
+
+const bowler = {
+    name: 'dane',
+    lastBowlingScore: 122
+}
